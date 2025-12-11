840fcdf7989d575348093379c985b634e16e3bab
Tim <tim.duke.314@gmail.com>
2025-12-11 14:16:04 -0600

    Updated stylesheet.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import "./style.css";
 import {autoLoadList} from "./modules/storage";
 import {loadFormButtons} from "./modules/formops";
 import {openNav, closeNav} from "./modules/offcanvasmenu.js";
+import menubtn from "./public/menubtn.svg";
 
 autoLoadList();
 
 loadFormButtons();
 
 let navToggle = document.getElementById("navToggle");
+navToggle.src = menubtn;
 navToggle.addEventListener("click", openNav);
 let closeBtn = document.getElementById("closeBtn");
 closeBtn.addEventListener("click", closeNav);
