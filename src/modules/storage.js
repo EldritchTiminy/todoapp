f3bddcb868cda937ca5525ed18ed0b27050f36b1
Tim <tim.duke.314@gmail.com>
2025-12-19 11:19:26 -0600

    Added documentation comments to storage.js

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,9 +1,28 @@
+/*
+this script handles the functionality for storing the users tasks in local storage, retrieving stored tasks, and downloading a list to the user's device
+
+function overview:
+- saveList() - grabs the current list of tasks from the taskLibrary object (tasks.js, line 2), the current autoSave boolean (tasks.js, line 48), puts them in an array together, stringifies the array with serialize() (serializer.js, line 1), and then saves the stringified array to localStorage
+
+- loadList () - retrieves the array stored in localStorage, parses it with deserialize() (serializer, line 37), splits the array into its two parts, then saves the list and autosave boolean to local taskLibrary
+
+- autoLoadList() - checks if there is a saved list from a previous visit in localStorage, loads a few example tasks with exampleTasks() (examples.js, line 4) if there isn't a saved list
+
+- clearList() - sets the current task list to an empty array and saves it
+
+- autoSaveToggle() - checks current state of the autoSave boolean (tasks.js, line 48) and toggles it
+
+- downloadBlob() - grabs the current task list, saves it to a new Blob, then uses DOM manipulation to save it to the user's device.
+*/
+
+//imports
 import taskLibrary from "./tasks";
 import {renderList} from "./taskrender";
 import exampleTasks from "./examples";
 import {autoSaveSetter} from "./formops";
 import {serialize, deserialize} from "./serializer";
 
+// scripting
 function saveList () {
   const currentTaskList = taskLibrary.tasks;
   const autoSaveSetting = taskLibrary.autoSave;
@@ -70,4 +89,12 @@ function downloadBlob () {
   document.body.removeChild(link);
 };
 
-export {saveList, loadList, autoLoadList, clearList, autoSaveToggle, downloadBlob};
+// exports
+export {
+  saveList,
+  loadList,
+  autoLoadList,
+  clearList,
+  autoSaveToggle,
+  downloadBlob
+};
